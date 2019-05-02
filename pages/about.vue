597a3f545845c97e781d35abaaf3f2951882ae09
Prateek Madaan <prince.cenation@gmail.com>
2019-05-02 23:38:54 +0530

    Add files via upload

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>About Jokes</h1>
+    <p>This is an app that displays jokes that are fetched
+      from an API
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      // For SEO (this is because of the vue-meta package)
+      title: "About JokesApp",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for jokes app"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
